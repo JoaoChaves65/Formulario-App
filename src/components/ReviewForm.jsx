d1bbddc265408c9212fa90090b12c1fd114a8d98
JoaoChaves65 <joaomarcosgol2000@gmail.com>
2023-05-02 19:02:09 -0300

    Personalizando o componente ReviewForm, adicionando campos para deixar o comentário

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { BsFillEmojiHeartEyesFill, BsEmojiSmileFill, BsFillEmojiNeutralFill, BsFillEmojiFrownFill, BsEmojiNeutralFill, BsFillEmojiSmileFill} from "react-icons/bs"
 
 export function ReviewForm() {
   return (
-    <div>
-        <h2>ReviewForm</h2>
+    <div className="review-form">
+        <div className="form-control score-container">
+          <label className="radio-container">
+            <input type="radio" value="unsatisfied" name="review" required/>
+            <BsFillEmojiFrownFill/>
+            <p>Insatisfeito</p>
+          </label>
+          <label className="radio-container">
+            <input type="radio" value="neutral" name="review" required/>
+            <BsEmojiNeutralFill/>
+            <p>Poderia ser melhor</p>
+          </label>
+          <label className="radio-container">
+            <input type="radio" value="satisfied" name="review" required/>
+            <BsFillEmojiSmileFill/>
+            <p>Satisfeito</p>
+          </label>
+          <label className="radio-container">
+            <input type="radio" value="very_satisfied" name="review" required/>
+            <BsFillEmojiHeartEyesFill/>
+            <p>Muito satisfeito</p>
+          </label>
+        </div>
+        <div className="form-control">
+          <label htmlFor="comment">Comentário:</label>
+          <textarea name="comment" id="comment" placeholder="Conte como foi a sua experiência com o produto..." required></textarea>
+        </div>
     </div>
   );
 };
